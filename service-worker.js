ddd48ad518068a4a867eb093ecf0bee1aec0acd8
surume99999 <surumeika40@gmail.com>
2026-06-06 15:50:43 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,21 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker installed");
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("todo-cache").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("Service Worker activated");
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
